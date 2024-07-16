93a7f3762acd8c98bfbe7dbfad17f25176b05cd5
TanNhat79 <nhoxtan11312@gmail.com>
2024-07-16 09:08:57 +0700

    final

---
+++ client-admin/src/components/ProductComponent.js
@@ -0,0 +1,120 @@
+import axios from 'axios';
+import React, { Component } from 'react';
+import MyContext from '../contexts/MyContext';
+import ProductDetail from './ProductDetailComponent';
+
+class Product extends Component {
+  static contextType = MyContext; // using this.context to access global state
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [],
+      noPages: 0,
+      curPage: 1,
+      itemSelected: null
+    };
+  }
+  render() {
+    const prods = this.state.products.map((item) => {
+      return (
+        <tr key={item._id} className="datatable" onClick={() => this.trItemClick(item)}>
+          <td>{item._id}</td>
+          <td>{item.name}</td>
+          <td>{item.price}</td>
+          <td>{new Date(item.cdate).toLocaleString()}</td>
+          <td>{item.category.name}</td>
+          <td><img src={"data:image/jpg;base64," + item.image} width="100px" height="100px" alt="" /></td>
+        </tr>
+      );
+    });
+    const pagination = Array.from({ length: this.state.noPages }, (_, index) => {
+      if ((index + 1) === this.state.curPage) {
+        return (<span key={index}>| <b>{index + 1}</b> |</span>);
+      } else {
+        return (<span key={index} className="link" onClick={() => this.lnkPageClick(index + 1)}>| {index + 1} |</span>);
+      }
+    });
+    return (
+      <div>
+        <div className="float-left">
+          <h2 className="text-center">PRODUCT LIST</h2>
+          <table className="datatable" border="1">
+            <tbody>
+              <tr className="datatable">
+                <th>ID</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Creation date</th>
+                <th>Category</th>
+                <th>Image</th>
+              </tr>
+              {prods}
+              <tr>
+                <td colSpan="6">{pagination}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className="inline" />
+        <ProductDetail item={this.state.itemSelected} curPage={this.state.curPage} updateProducts={this.updateProducts} />
+        <div className="float-clear" />
+      </div>
+    );
+  }
+  updateProducts = (products, noPages, curPage) => { // arrow-function
+    this.setState({ products: products, noPages: noPages, curPage: curPage });
+  }
+  // event-handlers
+  btnAddClick(e) {
+    e.preventDefault();
+    const name = this.state.txtName;
+    const price = parseInt(this.state.txtPrice);
+    const category = this.state.cmbCategory;
+    const image = this.state.imgProduct.replace(/^data:image\/[a-z]+;base64,/, ''); // remove "data:image/...;base64,"
+    if (name && price && category && image) {
+      const prod = { name: name, price: price, category: category, image: image };
+      this.apiPostProduct(prod);
+    } else {
+      alert('Please input name and price and category and image');
+    }
+  }
+  // apis
+  apiPostProduct(prod) {
+    const config = { headers: { 'x-access-token': this.context.token } };
+    axios.post('/api/admin/products', prod, config).then((res) => {
+      const result = res.data;
+      if (result) {
+        alert('OK BABY!');
+        this.apiGetProducts();
+      } else {
+        alert('SORRY BABY!');
+      }
+    });
+  }
+  apiGetProducts() {
+    const config = { headers: { 'x-access-token': this.context.token } };
+    axios.get('/api/admin/products?page=' + this.props.curPage, config).then((res) => {
+      const result = res.data;
+      this.props.updateProducts(result.products, result.noPages, result.curPage);
+    });
+  }
+  componentDidMount() {
+    this.apiGetProducts(this.state.curPage);
+  }
+  // event-handlers
+  lnkPageClick(index) {
+    this.apiGetProducts(index);
+  }
+  trItemClick(item) {
+    this.setState({ itemSelected: item });
+  }
+  // apis
+  apiGetProducts(page) {
+    const config = { headers: { 'x-access-token': this.context.token } };
+    axios.get('/api/admin/products?page=' + page, config).then((res) => {
+      const result = res.data;
+      this.setState({ products: result.products, noPages: result.noPages, curPage: result.curPage });
+    });
+  }
+}
+export default Product;
---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'laptrinhweb.fesqk7e.mongodb.net',
+    DB_USER: 'nhoxtan11312',
+    DB_PASS: '01265525442',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'vunhattan',
+    JWT_EXPIRES: '3600000', // in milliseconds
+    EMAIL_USER: 'nhoxtan11312@gmail.com', // gmail service
+    EMAIL_PASS: 'zusr bbvm bttp iegg'
+  };
+  module.exports = MyConstants;
